bfc6b00e8b7ab400f16ddfa2048982d300066af0
Jonathan Klaiber <joni.klaiber@gmx.de>
2016-05-02 01:52:49 +0200

    small fixes

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        // LOAD RES
+        // LOAD RESOURCES
         ///////////////////////////////////////////////////////////////////////////////////////////////////
         final FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
         Parent root = loader.load();
